75207fad830d1c8bd72307d2d15c56f4c6864111
David Bootle <d.bootle18@psaschool.org>
2020-04-28 11:11:22 -0400

    Added downloader homepage

--- app.js
+++ app.js
@@ -4,13 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var hike = require('./routes/hike');
-
 var app = express();
-app.get('/hikes', hike.index);
-app.post('/add_hike', hike.add_hike);
+
+var indexRouter = require('./routes/index');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +19,6 @@ app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
